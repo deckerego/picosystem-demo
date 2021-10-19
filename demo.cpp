d3f7494c134c7c8345d7316959e1decccdc904df
John Ellis <john@deckerego.net>
2021-10-18 22:41:55 -0400

    Reference spheres as pointers (update in-place)

--- demo.cpp
+++ demo.cpp
@@ -5,7 +5,7 @@
 using namespace blit;
 
 TileMap* environment;
-Sphere field[14][14];
+Sphere* field[14][14];
 
 void init() {
     set_screen_mode(ScreenMode::hires);
@@ -16,18 +16,31 @@ void init() {
 
 void init_field() {
   for(uint8_t x = 0; x < 14; ++x) {
+    field[x][13] = new Sphere(Vec2(8 + (x << 4), -16), rand() % 4);
+  }
+}
+
+void render_field() {
+  for(uint8_t x = 0; x < 14; ++x) {
     for(uint8_t y = 0; y < 14; ++y) {
-      field[x][y].position = Vec2(8 + (x << 4), y << 4);
+      Sphere* sphere = field[x][y];
+      if(sphere != nullptr) {
+        uint8_t sprite_idx = sphere->type;
+        screen.sprite(Rect(sprite_idx << 1, 0, 2, 2), sphere->position);
+      }
     }
   }
 }
 
-void blit_field() {
+void update_field() {
   for(uint8_t x = 0; x < 14; ++x) {
     for(uint8_t y = 0; y < 14; ++y) {
-      Sphere sphere = field[x][y];
-      uint8_t sprite_idx = sphere.type;
-      screen.sprite(Rect(sprite_idx << 1, 0, 2, 2), sphere.position);
+      Sphere* sphere = field[x][y];
+      if(sphere != nullptr) {
+        uint8_t expected_x = 8 + (x << 4);
+        uint8_t expected_y = y << 4;
+        if(sphere->position.y < expected_y) sphere->position.y += 2;
+      }
     }
   }
 }
@@ -40,7 +53,7 @@ void render(uint32_t time) { // millis elapsed since start
     screen.clear();
 
     environment->draw(&screen, Rect(0, 0, 240, 240), nullptr);
-    blit_field();
+    render_field();
 }
 
 void update(uint32_t time) { // millis elapsed since start
@@ -51,4 +64,6 @@ void update(uint32_t time) { // millis elapsed since start
   if (buttons.pressed & Button::DPAD_RIGHT) x_offset = 5;
   if (buttons.pressed & Button::DPAD_DOWN)  y_offset = 5;
   if (buttons.pressed & Button::DPAD_UP)    y_offset = -5;
+
+  update_field();
 }
--- demo.hpp
+++ demo.hpp
@@ -7,12 +7,12 @@ struct Sphere {
     blit::Vec2 position;
     uint8_t type = 0;
 
-    Sphere() : position(blit::Vec2(8, -16)), type(rand() % 4) {};
     Sphere(blit::Vec2 pos, uint8_t type) : position(pos), type(type) {};
 };
 
 void init_field();
-void blit_field();
+void render_field();
+void update_field();
 void init();
 void render(uint32_t time);
 void update(uint32_t time);
